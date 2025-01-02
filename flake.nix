9337fa26f7d94ee321a3b4303df1417561393183
Youssef <siryoussef@hotmail.com>
2025-01-02 11:06:28 +0200

    Revert "Update flake.nix"
    This reverts commit e09dd717bb2c8190f3f00f71d1f7030c97d455f4.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,5 @@
     nixosModules.impermanence = import ./nixos.nix;
     nixosModules.home-manager.impermanence = import ./home-manager.nix;
     nixosModule = self.nixosModules.impermanence;
-    homeManagerModules.impermanence = import ./home-manager.nix
   };
 }
